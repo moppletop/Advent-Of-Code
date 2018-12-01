0bb5b2fe1bfd30be7f93d9b9e089fa3184f2ee92
Sam <huzzahpinkyoshi@gmail.com>
2018-12-01 05:20:36 +0000

    Day 1 - 2018

--- src/main/java/com/moppletop/aoc/puzzle2018/Day201801.java
+++ src/main/java/com/moppletop/aoc/puzzle2018/Day201801.java
@@ -1,11 +1,17 @@
 package com.moppletop.aoc.puzzle2018;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.moppletop.aoc.AbstractDay;
 
 public class Day201801 extends AbstractDay
 {
+
+	private List<Integer> _frequencies;
 
 	public Day201801() throws IOException
 	{
@@ -15,19 +21,39 @@ public class Day201801 extends AbstractDay
 	@Override
 	protected void preSolve()
 	{
-
+		_frequencies = inputList().stream()
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	protected Object solvePart1()
 	{
-		return null;
+		int result = 0;
+
+		for (int frequency : _frequencies)
+		{
+			result += frequency;
+		}
+
+		return result;
 	}
 
 	@Override
 	protected Object solvePart2()
 	{
-		return null;
-	}
+		Set<Integer> seen = new HashSet<>();
+		int result = 0;
+		int i = 0;
 
+		while (true)
+		{
+			result += _frequencies.get(i++ % _frequencies.size());
+
+			if (!seen.add(result))
+			{
+				return result;
+			}
+		}
+	}
 }
